4c6ee933f8c3f88bfe72c6491a945c4033fcee6a
alice dar bello <alicedarbello@hotmail.com>
2025-07-05 11:33:17 -0300

    "One unique form to edit and new, and partials added "

--- seeds/seedIndex.js
+++ seeds/seedIndex.js
@@ -17,10 +17,16 @@ const seedDB = async() => {
     await Campground.deleteMany({})
     for (let i=0; i < 50; i ++){
         const randomCities = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[randomCities].city}, ${cities[randomCities].state}`,
-            title: `${sample(descriptors)}, ${sample(places)}` 
+            title: `${sample(descriptors)}, ${sample(places)}` ,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum dolorum asperiores quis voluptatem saepe ad vel dolorem quos dolores ipsa consequatur vero, recusandae mollitia cum esse placeat cupiditate impedit unde. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum dolorum asperiores quis voluptatem saepe ad vel dolorem quos dolores ipsa consequatur vero, recusandae mollitia cum esse placeat cupiditate impedit unde.',
+            price
         })
+        
+            
         await camp.save()
     }
 }
